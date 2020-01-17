364f5d90e9d107c0d869e27d968d5f73373a488e
dowookims <kaei2574@gmail.com>
2020-01-17 11:53:33 +0900

    [UPDATE] calculate weeks and every date numbers

--- Part 2. JavaScript/Quest 08. Midterm project/js/index.js
+++ Part 2. JavaScript/Quest 08. Midterm project/js/index.js
@@ -1,5 +1,5 @@
-const c = new Calendar(new Date());
+const c = new Calendar(new Date(2020, 0, 1));
 
 const m = new Month(c.year, c.month, []);
 
-console.log(m);
+console.log(m.drawMonth());
--- Part 2. JavaScript/Quest 08. Midterm project/js/month.js
+++ Part 2. JavaScript/Quest 08. Midterm project/js/month.js
@@ -3,15 +3,35 @@ class Month {
     this.year = y;
     this.month = m;
     this.todos = todos;
+    this.lastMonthDate = null;
     this.totalDate = null;
-    this.firstDate = null;
+    this.firstDay = null;
     this.weeks = null;
+
     (() => {
+      const lastMonth = new Date(y, m, 0);
       const monthFirst = new Date(y, m, 1);
       const monthLast = new Date(y, m+1, 0);
+      const firstDay = monthFirst.getDay();
+      const lastDay = monthLast.getDay();
+
+      this.lastMonthDate = lastMonth.getDate();
       this.totalDate = monthLast.getDate();
-      this.firstDate = monthFirst.getDate();
+      this.firstDay = firstDay;
+      this.weeks = ((this.totalDate + firstDay + 6 - lastDay) / 7);
     })();
+  }
+
+  drawMonth() {
+    for (let i=1; i<=this.weeks * 7; i++) {
+      if (this.firstDay >= i){
+        console.log("asd", this.lastMonthDate - this.firstDay + i);
+      } else if(i - this.firstDay > this.totalDate){
+        console.log(i - this.firstDay - this.totalDate);
+      } else {
+        console.log(i - this.firstDay);
+      }
+    }
   }
 }
 
