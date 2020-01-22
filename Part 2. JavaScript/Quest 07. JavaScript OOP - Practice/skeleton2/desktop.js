6dbfda9c6f09a4353f67cc0e5dbfac97d1a3073e
dowookims <kaei2574@gmail.com>
2020-01-22 16:21:45 +0900

    [REFACTORING] openWindow refactoring.

--- Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton2/desktop.js
+++ Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton2/desktop.js
@@ -10,7 +10,6 @@ class Desktop {
         this.icons.forEach(icon => {
             icon.adjustPosition(0, this.iconCount * 120);
             this.iconCount++;
-
             this.dom.append(icon.dom);
         });
     }
@@ -41,24 +40,25 @@ class Folder {
         this.name = name;
         this._prepareDom();
         this.dndHandler = new DndHandler(this.dom);
-        this.window = null;
     }
 
     _prepareDom () {
         Icon.prototype._prepareDom.call(this);
         this.dom.classList.add('folder');
-        
         this._openWindow();
     }
 
     _openWindow () {
         this.dom.addEventListener('dblclick', () => {
             this.dndHandler.isMouseDown = false;
-            if (!this.window) {
-                this.window = new Window(this.name)
-                this.dom.append(this.window.dom);
-            }
-            else this.window.dom.style.display = "block";
+            if (!document.window) {
+                document.window = [];
+                document.windowCount = -1;
+            };
+            const window = new Window(this.name)
+            document.window.push(window);
+            document.windowCount++;
+            this.dom.append(window.dom);
         })
     }
     
@@ -67,6 +67,7 @@ class Folder {
     }
 }
 
+//  Event로 값 전달하는 방식으로 한번 만들어보기
 class Window {
     constructor (name) {
         this.name = name;
@@ -83,11 +84,16 @@ class Window {
         const windowTemplate = document.getElementById('window');
         const windowClone = document.importNode(windowTemplate.content, true);
         const windowDiv = windowClone.querySelector('.window');
+        
         windowClone.querySelector('.window-name').innerHTML = this.name;
         windowClone.querySelector('.window-close-span').addEventListener('click', () => { 
-            this.dom.style.display = 'none';
-        })
+            this.dom.parentNode.removeChild(this.dom);
+            document.window.pop(document.windowCount);
+            document.windowCount--;
+        });
+
         this.dom = windowDiv;
+        
     }
 
 }
