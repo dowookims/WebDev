b5b9786bf59eebafdb8a2f177a3d107ca100eef3
dowookims <kaei2574@gmail.com>
2020-02-04 16:43:14 +0900

    [UPDATE] Quest 11

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -1,5 +1,21 @@
 const http = require('http');
+const url = require('url');
+const qs = require('querystring');
 
 http.createServer((req, res) => {
 	// TODO: 이 곳을 채워넣으세요..!
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	const parseObject = url.parse(req.url);
+	const pathName = parseObject.pathname;
+	if (pathName === '/foo') {
+		let queryTexts = qs.parse(parseObject.query);
+		if (!queryTexts.bar) queryTexts.bar = '';
+		if (req.method === 'GET' || req.method === 'POST') {
+			res.end(`HELLO, ${queryTexts.bar}`);
+		};
+	}
+	else {
+		res.end("HELLO WORLD");
+	}
 }).listen(8080);
