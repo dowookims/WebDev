1097b4de2cf85abd7be697164211a02124509011
dowookims <kaei2574@gmail.com>
2020-02-06 09:16:39 +0900

    [UPDATE] HTTP request object

--- Part 3. node.js/Quest 11. My little web server/skeleton/server.js
+++ Part 3. node.js/Quest 11. My little web server/skeleton/server.js
@@ -4,13 +4,18 @@ const qs = require('querystring');
 
 http.createServer((req, res) => {
 	// TODO: 이 곳을 채워넣으세요..!
+	req.on('connect', (req, cs, head) => {
+		console.log(head)
+	});
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
 	const parseObject = url.parse(req.url);
 	const pathName = parseObject.pathname;
 	if (pathName === '/foo') {
 		let queryTexts = qs.parse(parseObject.query);
+
 		if (!queryTexts.bar) queryTexts.bar = '';
+
 		if (req.method === 'GET' || req.method === 'POST') {
 			res.end(`HELLO, ${queryTexts.bar}`);
 		};
