719b74f9e9cca904f28e2897bb8daa6f6961c540
dowookims <kaei2574@gmail.com>
2020-02-13 12:44:09 +0900

    [UPDATE] tab content (just dom code)

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,11 +1,13 @@
 class Notepad {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
 		constructor(){
+			this.tabs = []
 			this._prepareDOM()
 		}
 
 		_prepareDOM(){
-			const menu = document.querySelector('.icon-div');
+			const iconDiv = document.querySelector('.icon-div');
+			const tapDiv = document.querySelector('.tab-div');
 
 			const iconArr = [
 				{name: 'new', type: 'create'},
@@ -14,9 +16,19 @@ class Notepad {
 			];
 			const iconInstances = iconArr.map(icon => {
 						const iconItem = new Icon(icon.name, icon.type)
-						menu.append(iconItem.dom);
+						iconDiv.append(iconItem.dom);
 					}
 			);
+
+			if (this.tabs.length === 0) {
+					const newTab = new Tab(1, 'untitled', '');
+					tapDiv.append(newTab.dom);
+			} else {
+					this.tabs.forEach(tab => {
+						const tabItem = new Tab(tab.id, tab.name, tab.text);
+						tapDiv.append(tabItem.dom);
+					})
+			}
 		}
 };
 
@@ -40,9 +52,21 @@ class Icon {
 };
 
 class Tab {
-		constructor(){}
+		constructor(id, name, text){
+			this.id = id;
+			this.name = name;
+			this.text = text;
+			this.dom = null;
+			this._prepareDOM();
+		}
 		
-		_prepareDOM(){}
+		_prepareDOM(){
+				const tabTemplate = document.getElementById('tab');
+				const tabClone = document.importNode(tabTemplate.content, true);
+				const tabNameSpan = tabClone.querySelector('.tab-name');
+				tabNameSpan.innerHTML = this.name;
+				this.dom = tabClone.querySelector('.tab');
+		}
 
 		_closeTab(){}
 };
