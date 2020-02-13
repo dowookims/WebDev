f1242e46856e9ee6a7876728bb54d8837f153e2e
dowookims <kaei2574@gmail.com>
2020-02-13 13:17:31 +0900

    [UPDATE] Board component

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -6,8 +6,10 @@ class Notepad {
 		}
 
 		_prepareDOM(){
+			const app = document.getElementById('app');
 			const iconDiv = document.querySelector('.icon-div');
 			const tapDiv = document.querySelector('.tab-div');
+			const boardDiv = document.querySelector('.board-div');
 
 			const iconArr = [
 				{name: 'new', type: 'create'},
@@ -28,7 +30,10 @@ class Notepad {
 						const tabItem = new Tab(tab.id, tab.name, tab.text);
 						tapDiv.append(tabItem.dom);
 					})
-			}
+			};
+
+			const boardInstance = new Board();
+			app.append(boardInstance.dom);
 		}
 };
 
@@ -46,9 +51,21 @@ class Icon {
 				const iconNameSpan = iconClone.querySelector('.icon-name');
 				iconNameSpan.innerHTML = this.name;
 				this.dom = iconClone.querySelector('.icon');
+				this.dom.addEventListener('click', () => this._clickEvent())
 		}
 
-		clickEvent(){}
+		_clickEvent(){
+				if (this.type === 'create') {
+						const tabDiv = document.querySelector('.tab-div');
+						const tabItem = new Tab(1, 'untitled', '');
+						tabDiv.append(tabItem.dom);
+				} else if (this.type ==='load') {
+						console.log("LOAD");
+				} else if (this.type === 'save') {
+						console.log("SAVE");
+				} 
+				
+		}
 };
 
 class Tab {
@@ -60,19 +77,31 @@ class Tab {
 			this._prepareDOM();
 		}
 		
-		_prepareDOM(){
+		_prepareDOM() {
 				const tabTemplate = document.getElementById('tab');
 				const tabClone = document.importNode(tabTemplate.content, true);
 				const tabNameSpan = tabClone.querySelector('.tab-name');
+				const closeTabSpan = tabClone.querySelector('.close-tab');
 				tabNameSpan.innerHTML = this.name;
 				this.dom = tabClone.querySelector('.tab');
-		}
 
-		_closeTab(){}
+				closeTabSpan.addEventListener('click', () => {
+						this.dom.remove();
+				})
+		}
 };
 
 class Board {
-		constructor(){}
+		constructor(text=""){
+			this.text =text;
+			this.dom = null;
+			this._prepareDOM();
+		}
 		
-		_prepareDOM(){}
+		_prepareDOM(){
+			const textAreaTemplate = document.getElementById('board');
+			const textAreaClone = document.importNode(textAreaTemplate.content, true);
+			const textAreaDiv = textAreaClone.querySelector('.board');
+			this.dom = textAreaDiv;
+		}
 };
