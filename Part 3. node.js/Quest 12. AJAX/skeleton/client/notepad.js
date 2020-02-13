1f705c270778bc9c078be275503a3fc57a88ab91
dowookims <kaei2574@gmail.com>
2020-02-13 16:33:21 +0900

    [UPDATE] making custom event to handle child component can fire event

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,40 +1,56 @@
 class Notepad {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
 		constructor(){
-			this.tabs = []
-			this._prepareDOM()
+			this.tabs = [];
+			this.currentTab = null;
+			this._prepareDOM();
+			this._customEventHandler();
 		}
 
 		_prepareDOM(){
 			const app = document.getElementById('app');
 			const iconDiv = document.querySelector('.icon-div');
-			const tapDiv = document.querySelector('.tab-div');
-			const boardDiv = document.querySelector('.board-div');
 
 			const iconArr = [
 				{name: 'new', type: 'create'},
 				{name: 'load', type: 'load'},
 				{name: 'save', type: 'save'}
 			];
+
+			// icon initiating
 			const iconInstances = iconArr.map(icon => {
 						const iconItem = new Icon(icon.name, icon.type)
 						iconDiv.append(iconItem.dom);
 					}
 			);
-
+			
+			// tab init
 			if (this.tabs.length === 0) {
-					const newTab = new Tab(1, 'untitled', '');
-					tapDiv.append(newTab.dom);
+					const newTab = Tab.create('untitled', '');
+					this.tabs.push(newTab);
 			} else {
 					this.tabs.forEach(tab => {
-						const tabItem = new Tab(tab.id, tab.name, tab.text);
-						tapDiv.append(tabItem.dom);
+						const tabItem = Tab.create(tab.name, tab.text, tab.id);
+						this.tabs.push(tabItem);
 					})
 			};
 
+			this.currentTab = this.tabs[this.tabs.length-1];
+
 			const boardInstance = new Board();
 			app.append(boardInstance.dom);
+			this.dom = app;
 		}
+
+		_customEventHandler() {
+				this.dom.addEventListener('removeTab', (e) => {
+					console.log(e.detail.id())
+						this.tabs =  this.tabs.filter(tab => {
+						tab.id !== e.detail.id
+				})
+				console.log(this.tabs);
+		})
+	}
 };
 
 class Icon {
@@ -57,19 +73,20 @@ class Icon {
 		_clickEvent(){
 				if (this.type === 'create') {
 						const tabDiv = document.querySelector('.tab-div');
-						const tabItem = new Tab(1, 'untitled', '');
+						const tabItem = new Tab('untitled', '');
 						tabDiv.append(tabItem.dom);
 				} else if (this.type ==='load') {
 						console.log("LOAD");
 				} else if (this.type === 'save') {
-						console.log("SAVE");
+						const textData = document.getElementById("textarea").value;
+						console.log(textData);
 				} 
 				
 		}
 };
 
 class Tab {
-		constructor(id, name, text){
+		constructor(name, text, id = new Date()){
 			this.id = id;
 			this.name = name;
 			this.text = text;
@@ -81,12 +98,42 @@ class Tab {
 				const tabTemplate = document.getElementById('tab');
 				const tabClone = document.importNode(tabTemplate.content, true);
 				const tabNameSpan = tabClone.querySelector('.tab-name');
-				const closeTabSpan = tabClone.querySelector('.close-tab');
 				tabNameSpan.innerHTML = this.name;
 				this.dom = tabClone.querySelector('.tab');
+				this._removeEvent();
+		}
 
-				closeTabSpan.addEventListener('click', () => {
-						this.dom.remove();
+		static create(name, text) {
+			const tapDiv = document.querySelector('.tab-div');
+			const tabItem = new Tab(name, text);
+			const createTab = new CustomEvent('createTab', {
+				bubles: true,
+				detail: {
+					id: () => tabItem.id,
+					name: () => tabItem.name,
+					text: () => tabItem.text
+				}
+			});
+			app.dispatchEvent(createTab);
+			tapDiv.append(tabItem.dom);
+			return tabItem
+		}
+
+		_removeEvent() {
+				this.dom.querySelector('.close-tab').addEventListener('click', e => {
+					this.dom.remove();
+					const removeTab = new CustomEvent('removeTab', {
+						bubbles: true,
+						detail: {id: () => this.id}
+					});
+					app.dispatchEvent(removeTab);
+				})
+		};
+
+		_openEvent() {
+				const openTab = new CustomEvent('openTab', {
+					bubles: true,
+					detail: {}
 				})
 		}
 };
