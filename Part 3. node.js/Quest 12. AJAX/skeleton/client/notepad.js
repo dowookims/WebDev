3a57ae55d37e578258649f30ffa86e1f41fa54af
dowookims <kaei2574@gmail.com>
2020-02-13 12:24:10 +0900

    [UPDATE] making basic dom

--- Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/client/notepad.js
@@ -1,3 +1,54 @@
 class Notepad {
 	/* TODO: 그 외에 또 어떤 클래스와 메소드가 정의되어야 할까요? */
+		constructor(){
+			this._prepareDOM()
+		}
+
+		_prepareDOM(){
+			const menu = document.querySelector('.icon-div');
+
+			const iconArr = [
+				{name: 'new', type: 'create'},
+				{name: 'load', type: 'load'},
+				{name: 'save', type: 'save'}
+			];
+			const iconInstances = iconArr.map(icon => {
+						const iconItem = new Icon(icon.name, icon.type)
+						menu.append(iconItem.dom);
+					}
+			);
+		}
+};
+
+class Icon {
+		constructor(name,type){
+				this.name = name;
+				this.type = type;
+				this.dom = null;
+				this._prepareDOM()
+		}
+		
+		_prepareDOM(){
+				const iconTemplate = document.getElementById('icon');
+				const iconClone = document.importNode(iconTemplate.content, true);
+				const iconNameSpan = iconClone.querySelector('.icon-name');
+				iconNameSpan.innerHTML = this.name;
+				this.dom = iconClone.querySelector('.icon');
+		}
+
+		clickEvent(){}
+};
+
+class Tab {
+		constructor(){}
+		
+		_prepareDOM(){}
+
+		_closeTab(){}
+};
+
+class Board {
+		constructor(){}
+		
+		_prepareDOM(){}
 };
